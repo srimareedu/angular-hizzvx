02a4ae711155439ae1e30748d756d0ab2b9a3435
srimareedu <srimareedu1@gmail.com>
2019-12-27 13:28:40 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
